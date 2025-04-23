1740cc8f150aec0ffa21cc478764804c5be4db4b
gr-arm-7xx2 <xiong.xiao@geometryrobot.com>
2025-04-23 15:19:47 +0800

    Initial commit

---
+++ cmake-build-debug/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/gr-arm-7xx2/gr3_arm_ros/catkin_ws/src/moveit_servo/cmake-build-debug/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/gr-arm-7xx2/gr3_arm_ros/catkin_ws/src/moveit_servo/cmake-build-debug/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/gr-arm-7xx2/gr3_arm_ros/catkin_ws/src/moveit_servo/cmake-build-debug/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ROSLISP_PACKAGE_DIRECTORIES='/home/gr-arm-7xx2/gr3_arm_ros/catkin_ws/src/moveit_servo/cmake-build-debug/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/gr-arm-7xx2/gr3_arm_ros/catkin_ws/src/moveit_servo:$ROS_PACKAGE_PATH"
